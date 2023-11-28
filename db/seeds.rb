f67a7e139d0d846c3e8fab0e418ccf4558424255
Yohan Gouiran <y.gouiran@gmail.com>
2023-11-28 17:10:30 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+
+10.times {
+  Article.create(
+    title: Faker::Commerce.brand,
+    content: Faker::Lorem.paragraph(sentence_count: 4, supplemental: false, random_sentences_to_add: 6)
+  )
+};
